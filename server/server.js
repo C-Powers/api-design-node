0c976dc34896cf923567990669a92949acd5f690
C Powers <cjpowers3@gmail.com>
2016-10-26 23:49:27 -0700

    example 2 completed

--- server/server.js
+++ server/server.js
@@ -22,6 +22,56 @@ var lions = [];
 var id = 0;
 
 // TODO: make the REST routes to perform CRUD on lions
+app.get('/lions', function(req, res) {
+  res.status(200).json(lions);
+});
+
+app.get('/lions/:id', function(req,res) {
+  //another option is to do var lion = _.find(lions,{id: req.params.id})
+  console.log(req.params.id);
+  lions.map(function(lion){
+    if(lion.id == req.params.id) {
+      res.json(lion);
+    }
+  });
+});
+
+app.post('/lions', function(req, res) {
+  var lion = req.body;
+  id++;
+  lion.id = id + '';
+
+  lions.push(lion);
+
+  res.json(lion);
+})
+
+app.put('/lions/:id', function(req, res) {
+  var update = req.body;
+  if (update.id) {
+    delete update.id
+  }
+
+  var lion = _.findIndex(lions, {id: req.params.id}); //lion is an index
+  if(!lions[lion]) {
+    res.send();
+  } else {
+    var updatedLion = _.assign(lions[lion], update);
+    res.json(updatedLion);
+  }
+});
+
+app.delete('/lions/:id', function(req, res) {
+  var lion = _.findIndex(lions, {id: req.params.id}); //lion is an index
+  if(!lions[lion]) {
+    res.send();
+  } else {
+    var deletedLion = lions[lion]
+    lions.splice(lion, 1);
+    res.json(deletedLion)
+  }
+})
+
 
 app.listen(3000);
 console.log('on port 3000');
