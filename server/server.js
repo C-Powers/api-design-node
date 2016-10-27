5bf694168a19ad14c54d7dd637ea10aafe6a620e
C Powers <cjpowers3@gmail.com>
2016-10-27 07:43:43 -0700

    begin ex 3

--- server/server.js
+++ server/server.js
@@ -1,50 +1,70 @@
-// TODO: make this work.
-// if yuo go to localhost:3000 the app
-// there is expected crud to be working here
+// TODO: user app.params to find the lion using the id
+// and then attach the lion to the req object and call next. Then in
+// '/lion/:id' just send back req.lion
+
+// create a middleware function to catch and handle errors, register it
+// as the last middleware on app
+
+
+// create a route middleware for POST /lions that will increment and
+// add an id to the incoming new lion object on req.body
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var _ = require('lodash');
+var morgan = require('morgan');
 
-// express.static will serve everything
-// with in client as a static resource
-// also, it will server the index.html on the
-// root of that directory on a GET to '/'
+//var lions = [];
+var lions = [
+  {"name": "test lion",
+  "id": 1,
+  "age": 1,
+  "pride": "testers",
+  "gender": "female"
+  },
+  {"name": "test lion2",
+  "id": 2,
+  "age": 2,
+  "pride": "testers",
+  "gender": "male"
+  }
+];
+var id = 0;
+
+var updateId = function(req, res, next) {
+  // fill this out. this is the route middleware for the ids
+  // add id to req.body
+};
+
+app.use(morgan('dev'))
 app.use(express.static('client'));
-
-// body parser makes it possible to post JSON to the server
-// we can accss data we post on as req.body
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 
-var lions = [];
-var id = 0;
-
-// TODO: make the REST routes to perform CRUD on lions
-app.get('/lions', function(req, res) {
-  res.status(200).json(lions);
+app.param('id', function(req, res, next, id) {
+  // fill this out to find the lion based off the id
+  // and attach it to req.lion. Rember to call next()
 });
 
-app.get('/lions/:id', function(req,res) {
-  //another option is to do var lion = _.find(lions,{id: req.params.id})
-  console.log(req.params.id);
-  lions.map(function(lion){
-    if(lion.id == req.params.id) {
-      res.json(lion);
-    }
-  });
+app.get('/lions', function(req, res){
+  res.json(lions);
 });
 
-app.post('/lions', function(req, res) {
+app.get('/lions/:id', function(req, res){
+  // use req.lion
+  res.json(lion || {});
+});
+
+app.post('/lions', updateId, function(req, res) {
   var lion = req.body;
-  id++;
-  lion.id = id + '';
 
   lions.push(lion);
 
   res.json(lion);
-})
+});
+
 
 app.put('/lions/:id', function(req, res) {
   var update = req.body;
@@ -52,26 +72,14 @@ app.put('/lions/:id', function(req, res) {
     delete update.id
   }
 
-  var lion = _.findIndex(lions, {id: req.params.id}); //lion is an index
-  if(!lions[lion]) {
+  var lion = _.findIndex(lions, {id: req.params.id});
+  if (!lions[lion]) {
     res.send();
   } else {
     var updatedLion = _.assign(lions[lion], update);
     res.json(updatedLion);
   }
 });
-
-app.delete('/lions/:id', function(req, res) {
-  var lion = _.findIndex(lions, {id: req.params.id}); //lion is an index
-  if(!lions[lion]) {
-    res.send();
-  } else {
-    var deletedLion = lions[lion]
-    lions.splice(lion, 1);
-    res.json(deletedLion)
-  }
-})
-
 
 app.listen(3000);
 console.log('on port 3000');
