f4637c5344d32cc25a1b3e013b435afe97c2e032
C Powers <cjpowers3@gmail.com>
2016-11-20 12:56:33 -0800

    my solution to ex5. testing with supertest, chai, and mocha

--- server/spec.js
+++ server/spec.js
@@ -9,6 +9,7 @@ var expect = require('chai').expect;
 //  to run the test type mocha server/specs.js
 
 describe('[LIONS]', function () {
+
   it('should get all lions', function(done) {
     request(app)
       .get('/lions')
@@ -20,4 +21,74 @@ describe('[LIONS]', function () {
         done();
       })
   });
+
+  var lion = {
+    name: 'Mufasa',
+    age: 100,
+    pride: 'Evil Lions',
+    gender: 'male'
+  }
+
+  it('should create a create a new lion', function(done) {
+    request(app)
+      .post('/lions')
+      .send(lion)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(201)
+      .end(function(err, resp) {
+        expect(resp.body).to.be.an('object');
+        done();
+      })
+  });
+
+  it('should get a single lion', function(done) {
+    request(app)
+      .post('/lions')
+      .send(lion)
+      .set('Accept', 'application/json')
+      .end(function(err, resp) {
+        var singleLion = resp.body;
+        request(app)
+          .get('/lions/' + singleLion.id)
+          .end(function(err, resp) {
+            expect(resp.body).to.eql(singleLion);
+            done();
+          });
+      })
+  });
+
+  it('should delete a lion', function(done){
+    request(app)
+      .post('/lions')
+      .send(lion)
+      .set('Accept', 'application/json')
+      .end(function(err, resp) {
+        var deleteLion = resp.body;
+        request(app)
+          .delete('/lions/' + deleteLion.id)
+          .end(function(err, resp) {
+            expect(resp.body).to.eql(deleteLion);
+            done();
+          });
+      })
+  });
+
+  it('should update a lion', function(done){
+    request(app)
+      .post('/lions')
+      .send(lion)
+      .set('Accept', 'application/json')
+      .end(function(err, resp) {
+        var updateLion = resp.body;
+        request(app)
+          .put('/lions/' + updateLion.id)
+          .send({name: 'Scar'})
+          .end(function(err, resp) {
+            expect(resp.body.name).to.eql('Scar');
+            done();
+          })
+      })
+  });
+
 });
